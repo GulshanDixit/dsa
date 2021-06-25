a80ecccb0f21881c9a300e9277b923e3c7a981d8
Gulshan Dixit <gulshandixit@hotmail.com>
2021-06-25 14:57:37 +0530

    Modified way to calculate permutations
    Modified the way to calculate permutations, which bypasses the situation of integer overflow and the need to store factorials.
    
    Getting matching answers 0 till 35 as the problem on leetcode.
    Mismatching starts from 36.
    
    Yes! This implementation isn't dynamic programming. This is mathematical optimization.

--- Dynamic Programming/ClimbStair.cpp
+++ Dynamic Programming/ClimbStair.cpp
@@ -1,6 +1,45 @@
 #include<vector>
 using namespace std;
 
+/// <summary>
+/// It calculates the permutations of numbers where two distinct numers are involved
+/// </summary>
+/// <param name="total">sum of both counts</param>
+/// <param name="group1">higher count of the two</param>
+/// <param name="group2">lower count of the two</param>
+/// <returns></returns>
+float permutationsHelper(int total, int group1, int group2)
+{
+    //(factorials[total] / (factorials[group1] * factorials[group2]));
+    if (total == group1)
+    {
+        return 1;
+    }
+
+    //TODO: Add assert check for comparision of group1 and group2
+    float prm = total;
+    int num = total;
+    int dnm = group2;
+    while (num > group1+1)
+    {
+        prm *= (num - 1);
+
+        if (dnm > 1)
+        {
+            prm /= dnm;
+            --dnm;
+        }
+        --num;
+    }
+
+    while (dnm > 1)
+    {
+        prm /= dnm;
+        --dnm;
+    }
+
+    return prm;
+}
 
 int climbStair(int n)
 {
@@ -13,35 +52,36 @@ int climbStair(int n)
         m -= 2;
     }
 
-    int count = 1; // all ones
+    float count = 1; // all ones
     int ones = 0;
     if (m != 0)// is it odd or even
     {
         ones = 1;
     }
     
-    vector<int> factorials(n);// store factorial to prevent recalculations
-    m = 0;
-    while (m < n)
+    while (max2s > 0)
     {
-        if (m == 0)
+        if (max2s > ones)
         {
-            factorials[m] = 1;
+            count += permutationsHelper(max2s + ones, max2s, ones);
         }
         else
         {
-            factorials[m] = factorials[m - 1] * m;
+            count += permutationsHelper(max2s + ones, ones, max2s);
         }
-        ++m;
-    }
-
-    while (max2s > 0)
-    {
-        count += (factorials[max2s+ones] / (factorials[max2s] * factorials[ones]));
         --max2s;
         ones += 2;
     }
 
-    return count;
+    
+    int iCount = static_cast<int>(count);
+    if (count - iCount >= 0.5f)
+    {
+        return iCount + 1;
+    }
+    else
+    {
+        return iCount;
+    }
 }
 
--- Dynamic Programming/Source.cpp
+++ Dynamic Programming/Source.cpp
@@ -7,7 +7,11 @@ using namespace std;
 
 int main()
 {
-    cout << climbStair(3) << endl;
+    //cout << climbStair(3) << endl;
+    //cout << climbStair(17) << endl;
+    cout << climbStair(25) << endl;
+    cout << climbStair(35) << endl;
+    cout << climbStair(45) << endl;
 
     //cout << "Mai toh raste se ja raha tha." << endl;
 
